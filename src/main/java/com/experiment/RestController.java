bb52e115086c82f4cb28087a22b71f06fe1e1e3e
Rob Wood <rob.a.wood@gmail.com>
2016-05-13 16:15:05 -0600

    Add: /endpoint01, 01/02, 02?id

--- src/main/java/com/experiment/RestController.java
+++ src/main/java/com/experiment/RestController.java
@@ -10,8 +10,21 @@ public class RestController {
     public RestController() {
         port(8031);
 
-        get("/", (req, res) -> "REST Service 01");
+        get("/", (request, response) -> "Home: REST Service 01");
 
+        get("/endpoint01", (request, response) -> {
+           return "Results of endpoint01";
+        });
+
+        get("/endpoint01/02", (request, response) -> {
+            return "Results of endpoint01/02";
+        });
+
+        // Usage: /endpoint02?id=123
+        get("/endpoint02", "application/json", (request, response) -> {
+           String id = request.queryParams("id");
+           return "Results of endpoint02 for id (" + id + ")";
+        });
 
     }
 }
